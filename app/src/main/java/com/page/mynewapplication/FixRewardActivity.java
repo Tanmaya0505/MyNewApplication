6c33d05adf23085c26fdc898c0b86e72d35241e7
tanmayarout54@gmail.com <tanmayarout54@gmail.com>
2020-07-08 17:11:35 +0530

    my app

---
+++ app/src/main/java/com/page/mynewapplication/FixRewardActivity.java
@@ -0,0 +1,14 @@
+package com.page.mynewapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FixRewardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fix_reward);
+    }
+}
